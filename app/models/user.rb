03412b5b0de2f9f9cb7043ce04e95e6462d01dbb
Bobby <b.taslimi.h@gmail.com>
2016-12-04 16:15:00 -0500

    Friendship Methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     
     default_scope { order('id DESC') }
+    
   has_many :friendships, dependent: :destroy
     has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
@@ -14,7 +15,7 @@ class User < ActiveRecord::Base
   
   def self.create_user_from_omniauth(auth)
         create(
-          avatar: process_uri(auth['info']['image'] + "?width=9999"),
+          image: process_uri(auth['info']['image'] + "?width=9999"),
           email: auth['info']['email'],
           provider: auth['provider'],
           uid: auth['uid'],
@@ -26,6 +27,26 @@ class User < ActiveRecord::Base
         )
   end
   
+  def request_match(user_2)
+        self.friendships.create(friend: user_2)
+    end
+    
+    def accept_friendship
+    self.update_attributes(state: "active", friended_at: Time.now)
+  end
+  
+  def remove_match(user2)
+    
+        inverse_friendship = inverse_friendships.where(user_id: user2).first
+          
+          if inverse_friendship
+              self.inverse_friendships.where(user_id: user2).first.destroy
+          else
+              self.friendships.where(friend_id: user2).first.destroy
+          end
+    end
+    
+    
     private
 
     def self.process_uri(uri)
