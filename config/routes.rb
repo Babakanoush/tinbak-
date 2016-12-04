224b1e59bd3b50ca80b1928e4591ebe2c5fb073f
Bobby <b.taslimi.h@gmail.com>
2016-12-03 19:08:58 -0500

    Layout page design, Matches page design, Profile page design, Setting page design, User Model Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  resources :users do
+    member do
+      get 'profile'
+      get 'matches'
+    end
+  end
+#  get 'users/index'
+
+#  get 'users/edit'
+
+#  get 'users/profile'
+
+#  get 'users/matches'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
